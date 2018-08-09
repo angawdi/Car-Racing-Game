3ccb92fe66809c35d18d17ae50eb1bc77cb673ba
Sky <anvandp18@gmail.com>
2018-08-09 15:28:03 -0700

    make the car moving

--- js/main.js
+++ js/main.js
@@ -1,23 +1,104 @@
 
-var score, bttStart, playAnimation, start;
-var begin = 0;
+var score, start, playAnimation, start, speed, background, setup;
+var playGame = false;
+var keys = {
+	ArrowUp: false,
+	ArrowDown: false,
+	ArrowLeft: false,
+	ArrowRight: false
+}
 
 document.addEventListener('DOMContentLoaded', function(){
-	bttStart = document.getElementById('bttStart');
+	start = document.getElementById('bttStart');
+	document.addEventListener('keydown', pressKeyOn);
+	document.addEventListener('keyup', pressKeyOff);
+
+	speed = document.getElementById('speed');
 	score = document.getElementById('score');
-	bttStart.addEventListener('click', startGame);
+	playGame = true;
+	start.addEventListener('click', startGame);
+	background = document.getElementById('background');
+
 	playAnimation = requestAnimationFrame(play);
 	console.log(play.textContent);
+
 	
 });
 
-function play(){
 
-}
 
 function startGame(){
-	console.log('nice');
-	bttStart.style.display = 'none';
+	console.log('lets play');
+	start.style.display = 'none';
+	var temp = document.createElement('div');
+	temp.setAttribute('class', 'playerCar');
+	temp.x = 250;
+	temp.y = 300;
+	background.appendChild(temp);
+	playGame = true;
+
+	setup = {
+	element: temp,
+	speed: 1,
+	gameScore: 0,
+	winnerScore: 10,
+	// roadwidth: 250
+}
+
+}
+
+function play(){
+	if (playGame){
+		console.log('Game is played');
+		update();
+		///movement
+		if(keys.ArrowUp){
+			setup.element.y -= 1;
+		}
+		if(keys.ArrowDown){
+			setup.element.y += 1;
+		}
+		if(keys.ArrowRight){
+			setup.element.x += 1;
+		}
+		if(keys.ArrowLeft){
+			setup.element.x -= 1;
+		}
+
+		setup.element.style.top = setup.element.y + 'px';
+		setup.element.style.left = setup.element.x + 'px';
+	 }
+	playAnimation = requestAnimationFrame(play);
 }
 
 
+function pressKeyOn(event){
+	event.preventDefault();
+	console.log(event.key);
+	keys[event.key] = true;
+}
+
+function pressKeyOff(event){
+	event.preventDefault();
+	console.log(keys);
+	keys[event.key] = false;
+}
+
+
+
+function update(){
+	score.innerHTML = setup.gameScore;
+	speed.innerHTML = setup.speed;
+}
+
+function pressKeyOn(event){
+	event.preventDefault();
+	console.log(event.key);
+	keys[event.key] = true;
+}
+
+function pressKeyOff(event){
+	event.preventDefault();
+	console.log(keys);
+	keys[event.key] = false;
+}
